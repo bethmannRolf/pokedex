8df33d35c0ae054f4c267f1bdb039ff040814535
bethmannRolf <devak.robe@gmail.com>
2024-05-22 13:56:02 +0200

    finished styling main theme

--- script.js
+++ script.js
@@ -1,16 +1,32 @@
-let url = 'https://pokeapi.co/api/v2/pokemon?limit=20&offset=0'; 
+const BASE_URL = 'https://pokeapi.co/api/v2/'
+
+
+let url = 'https://pokeapi.co/api/v2/pokemon?limit=30&offset=0';
+
 let loadedPokemons = [];
 let DETAIL_VIEW_OPEN = false;
+let pokemonJSON = []
 
-function loadPokemon(){
+function loadPokemon() {
 
 
 }
 
+function additionalFunctionsonload() {
+    createPokemonCard()
+}
 
- async function initContext(){
-let response = await fetch(url)
-let responseASJSON = await response.json()
-console.log(responseASJSON)
 
+async function initContext() {
+    let response = await fetch(url)
+    let responseASJSON = await response.json()
+    console.log(responseASJSON)
+
+}
+
+
+function createPokemonCard() {
+    let cardContent = document.getElementById('single-card-main-content');
+    cardContent.innerHTML = `<span class="hallo-span"></span>`
+    
 }
